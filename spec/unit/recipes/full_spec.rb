b63d04ba6a853a938699a2a52688edc810b578e3
theekgb <reg.kgb@kgbbconcepts.com>
2018-04-16 19:45:59 -0400

    fixed kitchen test with cookbook pipeline

--- spec/unit/recipes/full_spec.rb
+++ spec/unit/recipes/full_spec.rb
@@ -6,7 +6,7 @@
 
 require 'spec_helper'
 
-describe 'pipeline::full' do
+describe 'pipeline::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
