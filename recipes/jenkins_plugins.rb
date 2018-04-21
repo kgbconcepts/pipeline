9d48c505c5296433a6c95172c4e5b7ddb3f450e9
theekgb <reg.kgb@kgbbconcepts.com>
2018-04-21 13:46:50 -0400

    gradle moved out to system

--- recipes/jenkins_plugins.rb
+++ recipes/jenkins_plugins.rb
@@ -29,8 +29,7 @@ end
 execute 'install_plugins' do
   command <<-EOH.gsub(/^ {4}/, '')
     #!/bin/bash
-    source /etc/profile
-    /opt/gradle/bin/gradle install && /opt/gradle/bin/gradle dependencies > 'plugins.lock'
+    gradle install && gradle dependencies > 'plugins.lock'
   EOH
   user node[jenkins_cb_name]['master']['user']
   group node[jenkins_cb_name]['master']['group']
